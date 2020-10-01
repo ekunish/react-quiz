2ad4d0a40cbf8a0f7158e318290a58c6e9a43c3f
ekunish <ekunish@gmail.com>
2020-10-01 17:37:23 +0900

    Updates

---
+++ precache-manifest.393a1074224c13c51c591743c774d8eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13b74913d478830f09ae5c2181a8d3f",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "6429d2a7204ad97ef961",
+    "url": "/react-quiz/static/js/2.286e6b6e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz/static/js/2.286e6b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e340818a1dc3531b3c",
+    "url": "/react-quiz/static/js/main.52a250b4.chunk.js"
+  },
+  {
+    "revision": "928f8d10e2d93c26f2dd",
+    "url": "/react-quiz/static/js/runtime-main.acabde0b.js"
+  },
+  {
+    "revision": "8ebbed4482b3aa22e8d3333f38f757d7",
+    "url": "/react-quiz/static/media/background.8ebbed44.jpg"
+  }
+]);
